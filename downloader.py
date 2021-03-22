bcc5ade9bb43a31b820ba24852fde99d3fd327f4
DiazRock <corolariodiaz@gmail.com>
2021-03-22 14:47:15 -0400

    Using a web server now

--- downloader.py
+++ downloader.py
@@ -1,10 +1,13 @@
+import os
 import requests
 from bs4 import BeautifulSoup
+from bottle import route, run, template
 
 PROVEYOURWORTH_URL= 'https://www.proveyourworth.net/level3/'
-CAR_PICTURE_PATH = 'car.jpg'
-RESUME_PATH = 'resume.pdf'
-CODE_PATH = 'downloader.py'
+CAR_PICTURE_PATH= 'car.jpg'
+RESUME_PATH= 'resume.pdf'
+CODE_PATH= 'downloader.py'
+TEMPLATE = ''
 
 def bobby_request(url):
     with requests.get(url) as resp_get:
@@ -19,43 +22,65 @@ def bobby_request(url):
 def angels_request(url, data, cookies):
     with requests.post(url, 
         data= data, 
-        cookies= cookies) as resp_post:
+        cookies= cookies) as resp_post,\
+        open('angels_content.html', 'wb') as ac,\
+        open('angels_headers.json', 'w') as ah:
+        ac.write(resp_post.content)
+        ah.write(repr(resp_post.headers))
         return resp_post.headers['X-Payload-URL']
 
 def car_request(payload_url, data, cookies):
     with requests.get(payload_url, data= data, cookies = cookies) as resp_get,\
-         open(CAR_PICTURE_PATH, 'wb') as car_picture:
+         open(CAR_PICTURE_PATH, 'wb') as car_picture,\
+         open('car_headers.json', 'w') as ch:
         car_picture.write(resp_get.content)
+        ch.write(repr(resp_get.headers))
         return resp_get.headers['X-Post-Back-To'], {'PHPSESSID': resp_get.cookies.get('PHPSESSID')}
         
+
 def final_upload(upload_url, statefulhash, cookies):
+    global TEMPLATE
     with open(CAR_PICTURE_PATH, 'rb') as cf,\
          open(RESUME_PATH, 'rb') as rf,\
          open(CODE_PATH, 'rb') as cdf:
-            data = {
-                'image': cf.read(),
-                'resume': rf.read(),
-                'code':  cdf.read(),
-                'statefulhash': statefulhash,
-                'name': 'Alejandro Díaz Roque',
-                'email': 'corolariodiaz@gmail.com',
-                'aboutme': "I'm a software engineer, with knowledge of Computer Science and Math. I made projects in I applied ideas of Language Theory, like a Compiler and a Context Free Grammar Annalyzer. I got knowledge of backend web technologies like docker, python and zmq. And I also have experience working with frontend web technologies as vue js, vuex, nuxt and react"    
+            files = {
+                'image': ('bmw_for_life.jpg', cf, 'multipart/form-data'),
+                'code': ('downloader.py', cdf, 'multipart/form-data'),
+                'resume': ('resume.pdf', rf, 'multipart/form-data'),
             }
-            with requests.post(upload_url, data= data, cookies= cookies) as resp_post,\
-                open('final_content.html', 'wb') as final:
-                final.write(resp_post.content)
+            data = {
+                
+                'statefulhash': statefulhash,
+                'name': 'Alejandro Diaz Roque',
+                'email': 'corolariodiaz@gmail.com',
+                'aboutme': "I'm a software engineer, with knowledge of Computer Science and Math. I made projects in I applied ideas of Language Theory, like a Compiler and a Context Free Grammar Annalyzer. I got knowledge of backend web technologies like docker, python and zmq. And I also have experience working with frontend web technologies as vue js, vuex, nuxt and react",
+                
+            }
+            
+            with requests.post(upload_url,
+                               data= data,
+                               files = files,
+                               cookies= cookies) as resp_post:
+                
+                TEMPLATE= template(repr(resp_post.content) + '\n' + repr(resp_post.headers) + '\n' + repr (resp_post.cookies))
+
+@route('/')
+def index():
+    return TEMPLATE
 
 def main():
     cookies, submit_data, form_method = bobby_request(url = PROVEYOURWORTH_URL)
     payload_url= angels_request(url= PROVEYOURWORTH_URL + '/' + form_method,
                                                 data = submit_data,
                                                 cookies= cookies)
+    
     upload_url, new_cookies= car_request(payload_url= payload_url,
                 data= submit_data,
                 cookies= cookies)
     final_upload(upload_url, 
                  statefulhash= submit_data['statefulhash'],
                  cookies= new_cookies)
+    run(host='localhost', port=9000)
     
 if __name__ == '__main__':
     main()
