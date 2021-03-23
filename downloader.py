e48163c4a1b52334db6a1990486e7418f1ed55b5
DiazRock <corolariodiaz@gmail.com>
2021-03-23 14:15:26 -0400

    Another try

--- downloader.py
+++ downloader.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
 
 PROVEYOURWORTH_URL= 'https://www.proveyourworth.net/level3/'
 CAR_PICTURE_PATH = 'car.jpg'
@@ -25,34 +26,36 @@ def angels_request(url, data, cookies):
         return resp_post.headers['X-Payload-URL']
 
 def car_request(payload_url, data, cookies):
-    with requests.get(payload_url, data= data, cookies = cookies) as resp_get,\
-         open(CAR_PICTURE_PATH, 'wb') as car_picture:
-        car_picture.write(resp_get.content)
-        return resp_get.headers['X-Post-Back-To'], {'PHPSESSID': resp_get.cookies.get('PHPSESSID')}
+    with requests.get(payload_url, data= data, cookies = cookies) as resp_get:
+        return resp_get.headers['X-Post-Back-To'], {'PHPSESSID': resp_get.cookies.get('PHPSESSID')}, resp_get.content
 
-def sign_payload(my_name, statefulhash):
-    with Image.open(CAR_PICTURE_PATH) as car_picture:
-        draw = ImageDraw.Draw(car_picture)
-        font = ImageFont.truetype('Roboto-Bold.ttf', size=30)
-        (x, y) = (25, 25)
-        color = 'rgb(49,90,200)'
-        message = '{0}\n{1}'.format(my_name, statefulhash)
-        draw.text((x, y), message, fill=color, font=font)
-        car_picture.save(SIGNED_CAR_PATH)
+def sign_payload(my_name, statefulhash, image):
+    stream = BytesIO(image)
+    image = Image.open(stream)
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype('Roboto-Bold.ttf', size=30)
+    (x, y) = (25, 25)
+    color = 'rgb(49,90,200)'
+    message = '{0}\n{1}'.format(my_name, statefulhash)
+    draw.text((x, y), message, fill=color, font=font)
+    image.save(SIGNED_CAR_PATH, "JPEG")
+
 
 def final_upload(upload_url, cookies):
-    with Image.open(SIGNED_CAR_PATH) as cf,\
+    with open(SIGNED_CAR_PATH, 'rb') as cf,\
          open(RESUME_PATH, 'rb') as rf,\
          open(CODE_PATH, 'rb') as cdf:
+            files = {
+                'image': cf,
+                'resume': rf,
+                'code':  cdf,
+            }
             data = {
-                'image': cf.tobytes(),
-                'resume': rf.read(),
-                'code':  cdf.read(),
                 'name': 'Alejandro Díaz Roque',
                 'email': 'corolariodiaz@gmail.com',
                 'aboutme': "I'm a software engineer, with knowledge of Computer Science and Math. I made projects in which I applied ideas of Language Theory, like a Compiler and a Context Free Grammar Annalyzer. I got knowledge of backend web technologies like docker, python and zmq. And I also have experience working with frontend web technologies as vue js, vuex, nuxt and react"    
             }
-            with requests.post(upload_url, data= data, cookies= cookies) as resp_post,\
+            with requests.post(upload_url, files = files, data= data, cookies= cookies) as resp_post,\
                  open('final_content.html', 'wb') as final_content:
                 print(resp_post.content)
                 final_content.write(resp_post.content)
@@ -63,11 +66,12 @@ def main():
     payload_url= angels_request(url= PROVEYOURWORTH_URL + '/' + form_method,
                                                 data = submit_data,
                                                 cookies= cookies)
-    upload_url, new_cookies= car_request(payload_url= payload_url,
+    upload_url, new_cookies, image= car_request(payload_url= payload_url,
                 data= submit_data,
                 cookies= cookies)
-    sign_payload(my_name= 'Alejandro Díaz Roque', 
-                 statefulhash= submit_data['statefulhash']) 
+    sign_payload(my_name= 'Alejandro Díaz Roque',
+                 statefulhash= submit_data['statefulhash'],
+                 image = image) 
     final_upload(upload_url,
                  cookies= new_cookies)
 if __name__ == '__main__':
