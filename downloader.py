b8663070a2ed827585812307d2f3985717ef3aae
DiazRock <corolariodiaz@gmail.com>
2021-03-23 13:09:12 -0400

    Signin Image

--- downloader.py
+++ downloader.py
@@ -1,13 +1,12 @@
-import os
 import requests
 from bs4 import BeautifulSoup
-from bottle import route, run, template
+from PIL import Image, ImageDraw, ImageFont
 
 PROVEYOURWORTH_URL= 'https://www.proveyourworth.net/level3/'
-CAR_PICTURE_PATH= 'car.jpg'
-RESUME_PATH= 'resume.pdf'
-CODE_PATH= 'downloader.py'
-TEMPLATE = ''
+CAR_PICTURE_PATH = 'car.jpg'
+SIGNED_CAR_PATH = 'signed.jpg'
+RESUME_PATH = 'resume.pdf'
+CODE_PATH = 'downloader.py'
 
 def bobby_request(url):
     with requests.get(url) as resp_get:
@@ -22,65 +21,54 @@ def bobby_request(url):
 def angels_request(url, data, cookies):
     with requests.post(url, 
         data= data, 
-        cookies= cookies) as resp_post,\
-        open('angels_content.html', 'wb') as ac,\
-        open('angels_headers.json', 'w') as ah:
-        ac.write(resp_post.content)
-        ah.write(repr(resp_post.headers))
+        cookies= cookies) as resp_post:
         return resp_post.headers['X-Payload-URL']
 
 def car_request(payload_url, data, cookies):
     with requests.get(payload_url, data= data, cookies = cookies) as resp_get,\
-         open(CAR_PICTURE_PATH, 'wb') as car_picture,\
-         open('car_headers.json', 'w') as ch:
+         open(CAR_PICTURE_PATH, 'wb') as car_picture:
         car_picture.write(resp_get.content)
-        ch.write(repr(resp_get.headers))
         return resp_get.headers['X-Post-Back-To'], {'PHPSESSID': resp_get.cookies.get('PHPSESSID')}
-        
 
-def final_upload(upload_url, statefulhash, cookies):
-    global TEMPLATE
-    with open(CAR_PICTURE_PATH, 'rb') as cf,\
+def sign_payload(my_name, statefulhash):
+    with Image.open(CAR_PICTURE_PATH) as car_picture:
+        draw = ImageDraw.Draw(car_picture)
+        font = ImageFont.truetype('Roboto-Bold.ttf', size=30)
+        (x, y) = (25, 25)
+        color = 'rgb(49,90,200)'
+        message = '{0}\n{1}'.format(my_name, statefulhash)
+        draw.text((x, y), message, fill=color, font=font)
+        car_picture.save(SIGNED_CAR_PATH)
+
+def final_upload(upload_url, cookies):
+    with Image.open(SIGNED_CAR_PATH) as cf,\
          open(RESUME_PATH, 'rb') as rf,\
          open(CODE_PATH, 'rb') as cdf:
-            files = {
-                'image': ('bmw_for_life.jpg', cf, 'multipart/form-data'),
-                'code': ('downloader.py', cdf, 'multipart/form-data'),
-                'resume': ('resume.pdf', rf, 'multipart/form-data'),
-            }
             data = {
-                
-                'statefulhash': statefulhash,
-                'name': 'Alejandro Diaz Roque',
+                'image': cf.tobytes(),
+                'resume': rf.read(),
+                'code':  cdf.read(),
+                'name': 'Alejandro Díaz Roque',
                 'email': 'corolariodiaz@gmail.com',
-                'aboutme': "I'm a software engineer, with knowledge of Computer Science and Math. I made projects in I applied ideas of Language Theory, like a Compiler and a Context Free Grammar Annalyzer. I got knowledge of backend web technologies like docker, python and zmq. And I also have experience working with frontend web technologies as vue js, vuex, nuxt and react",
-                
+                'aboutme': "I'm a software engineer, with knowledge of Computer Science and Math. I made projects in which I applied ideas of Language Theory, like a Compiler and a Context Free Grammar Annalyzer. I got knowledge of backend web technologies like docker, python and zmq. And I also have experience working with frontend web technologies as vue js, vuex, nuxt and react"    
             }
-            
-            with requests.post(upload_url,
-                               data= data,
-                               files = files,
-                               cookies= cookies) as resp_post:
+            with requests.post(upload_url, data= data, cookies= cookies) as resp_post,\
+                 open('final_content.html', 'wb') as final_content:
+                print(resp_post.content)
+                final_content.write(resp_post.content)
                 
-                TEMPLATE= template(repr(resp_post.content) + '\n' + repr(resp_post.headers) + '\n' + repr (resp_post.cookies))
-
-@route('/')
-def index():
-    return TEMPLATE
 
 def main():
     cookies, submit_data, form_method = bobby_request(url = PROVEYOURWORTH_URL)
     payload_url= angels_request(url= PROVEYOURWORTH_URL + '/' + form_method,
                                                 data = submit_data,
                                                 cookies= cookies)
-    
     upload_url, new_cookies= car_request(payload_url= payload_url,
                 data= submit_data,
                 cookies= cookies)
-    final_upload(upload_url, 
-                 statefulhash= submit_data['statefulhash'],
+    sign_payload(my_name= 'Alejandro Díaz Roque', 
+                 statefulhash= submit_data['statefulhash']) 
+    final_upload(upload_url,
                  cookies= new_cookies)
-    run(host='https://application-work.herokuapp.com', port=9000)
-    
 if __name__ == '__main__':
     main()
